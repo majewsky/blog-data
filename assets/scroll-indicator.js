5972204a5708e555cc10c1825ae3ae5d17aa33e1
Stefan Majewsky <stefan.majewsky@sap.com>
2017-04-21 11:18:39 +0200

    fix scroll-indicator in Safari

--- assets/scroll-indicator.js
+++ assets/scroll-indicator.js
@@ -2,7 +2,8 @@
   var si = document.querySelector(".scroll-indicator");
   var onscroll = function() {
     var max = document.body.scrollHeight - window.innerHeight;
-    si.style = "display:block;width:" + (window.pageYOffset / max * 100) + "%";
+    si.style.display = "block";
+    si.style.width = (window.pageYOffset / max * 100) + "%";
   };
 
   window.onscroll = onscroll;
